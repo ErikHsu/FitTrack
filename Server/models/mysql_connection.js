6b1aad4f0bf53193296866a97d2a41f99695f03d
Erik <ehsu820@gmail.com>
2019-03-14 00:34:03 -0400

    setup basic routes and views

--- Server/models/mysql_connection.js
+++ Server/models/mysql_connection.js
@@ -5,7 +5,7 @@ const mysql = require('mysql')
 const conn = mysql.createPool({
     host: process.env.VUE_APP_MYSQL_HOST,
     user: process.env.VUE_APP_MYSQL_USER,
-    password: process.env.VUE_APP_MYSQL_PASSWORD, 
+    password: process.env.VUE_APP_MYSQL_PASSWORD,
     database: process.env.VUE_APP_MYSQL_DB,
     port: 3307,
     connectionLimit: 10,
--- src/router.js
+++ src/router.js
@@ -20,6 +20,48 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    //User Profile
+    {
+      path: '/profile',
+      name: 'profile',
+      component: () => import('./views/Profile.vue')
+    },
+    //Registration
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('./views/Register.vue')
+    },
+    //Profile Settings
+    {
+      path: '/settings',
+      name: 'settings',
+      component: () => import('./views/Settings.vue')
+    },
+    //Body History
+    {
+      path: '/bodyHistory',
+      name: 'body history',
+      component: () => import('./views/BodyHistory.vue')
+    },
+    //Food Nutrition
+    {
+      path: '/nutrition',
+      name: 'nutrition',
+      component: () => import('./views/Nutrition.vue')
+    },
+    //Exercise Plans
+    {
+      path: '/plans',
+      name: 'plans',
+      component: () => import('./views/Plans.vue')
+    },
+    //Exercise List
+    {
+      path: '/exercises',
+      name: 'exercises',
+      component: () => import('./views/Exercises.vue')
     }
   ]
 })
