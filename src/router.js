99454be52b08789a1841ba27291d75da36e29060
Erik Hsu <ehsu820@gmail.com>
2019-04-18 14:15:06 -0400

    fixed npm run serve

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Register from './views/Register.vue'
 
 Vue.use(Router)
 
@@ -21,47 +22,11 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     },
-    //User Profile
-    {
-      path: '/profile',
-      name: 'profile',
-      component: () => import('./views/Profile.vue')
-    },
     //Registration
     {
       path: '/register',
       name: 'register',
-      component: () => import('./views/Register.vue')
-    },
-    //Profile Settings
-    {
-      path: '/settings',
-      name: 'settings',
-      component: () => import('./views/Settings.vue')
-    },
-    //Body History
-    {
-      path: '/bodyHistory',
-      name: 'body history',
-      component: () => import('./views/BodyHistory.vue')
-    },
-    //Food Nutrition
-    {
-      path: '/nutrition',
-      name: 'nutrition',
-      component: () => import('./views/Nutrition.vue')
-    },
-    //Exercise Plans
-    {
-      path: '/plans',
-      name: 'plans',
-      component: () => import('./views/Plans.vue')
-    },
-    //Exercise List
-    {
-      path: '/exercises',
-      name: 'exercises',
-      component: () => import('./views/Exercises.vue')
+      component: Register
     }
   ]
 })
